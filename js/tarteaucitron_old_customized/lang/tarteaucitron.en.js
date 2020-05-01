16cfc756201da94c5bad91fee2a69efb58a6394c
Armel, WAKAM <wakspace@gmail.com>
2020-05-01 20:29:07 +0200

    ReRecommit car ajout dans le stage avant Recommit

---
+++ js/tarteaucitron_old_customized/lang/tarteaucitron.en.js
@@ -0,0 +1,62 @@
+/*global tarteaucitron */
+tarteaucitron.lang = {
+    "middleBarHead": "☝ 🍪",
+    "adblock": "Hello! This site is transparent and lets you chose the 3rd party services you want to allow.",
+    "adblock_call": "Please disable your adblocker to start customizing.",
+    "reload": "Refresh the page",
+    
+    "alertBigScroll": "By continuing to scroll,",
+    "alertBigClick": "If you continue to browse this website,",
+    "alertBig": "you are allowing all third-party services",
+    
+    "alertBigPrivacy": "Notre site utilise des cookies. En poursuivant votre navigation, vous acceptez l’utilisation de cookies destinés à améliorer la performance et les fonctionnalités de ce site, à nous permettre de réaliser des statistiques  et à vous proposer du contenu et des offres et services adaptés à vos centres d’intérêts. ",
+    "alertSmall": "Manage services",
+    "personalize": "Personalize",
+    "acceptAll": "OK, accept all",
+    "close": "Close",
+	"valider": "Ok",
+
+    "privacyUrl": "Privacy policy",
+    
+    "all": "Preference for all services",
+
+    "info": "Protecting your privacy",
+    "disclaimer": "By allowing these third party services, you accept their cookies and the use of tracking technologies necessary for their proper functioning.",
+    "allow": "Allow",
+    "deny": "Deny",
+    "noCookie": "This service does not use cookie.",
+    "useCookie": "This service can install",
+    "useCookieCurrent": "This service has installed",
+    "useNoCookie": "This service has not installed any cookie.",
+    "more": "Read more",
+    "source": "View the official website",
+    "credit": "Cookies manager by Adis",
+    "noServices": "This website does not use any cookie requiring your consent.",
+
+    "toggleInfoBox": "Show/hide informations about cookie storage",
+    "title": "Cookies management panel",
+    "cookieDetail": "Cookie detail for",
+    "ourSite": "on our site",
+    "newWindow": "(new window)",
+    "allowAll": "Allow all cookies",
+    "denyAll": "Deny all cookies",
+    
+    "fallback": "is disabled.",
+
+    "necessaires": {
+        "title": "Nécessaires",
+        "details": "Les cookies nécessaires contribuent à rendre un site Web utilisable en activant des fonctions de base comme la navigation de page et l'accès aux zones sécurisées du site Web. Le site Web ne peut pas fonctionner correctement sans ces cookies."
+    },
+    "statistiques": {
+        "title": "Statistiques",
+        "details": "Les cookies statistiques aident les propriétaires du site Web, par la collecte et la communication d'informations de manière anonyme, à comprendre comment les visiteurs interagissent avec les sites Web."
+    },
+    "marketing": {
+        "title": "Marketing",
+        "details": "Les cookies marketing sont utilisés pour effectuer le suivi des visiteurs au travers des sites Web. Le but est d'afficher des publicités qui sont pertinentes et intéressantes pour l'utilisateur individuel et donc plus précieuses pour les éditeurs et annonceurs tiers."
+    },
+    "preferences": {
+        "title": "Préférences",
+        "details": "Les cookies de préférences permettent à un site Web de retenir des informations qui modifient la manière dont le site se comporte ou s’affiche, comme votre langue préférée ou la région dans laquelle vous vous situez."
+    }
+};
---
+++ js/tarteaucitron_old_customized/lang/tarteaucitron.fr.js
@@ -0,0 +1,62 @@
+/*global tarteaucitron */
+tarteaucitron.lang = {
+
+    "middleBarHead": "☝ 🍪",
+    "adblock": "Bonjour! Ce site joue la transparence et vous donne le choix des services tiers à activer.",
+    "adblock_call": "Merci de désactiver votre adblocker pour commencer la personnalisation.",
+    "reload": "Recharger la page",
+    
+    "alertBigScroll": "En continuant de défiler,",
+    "alertBigClick": "En poursuivant votre navigation,",
+    "alertBig": "vous acceptez l'utilisation de services tiers pouvant installer des cookies",
+    
+    "alertBigPrivacy": "Notre site utilise des cookies. En poursuivant votre navigation, vous acceptez l’utilisation de cookies destinés à améliorer la performance et les fonctionnalités de ce site, à nous permettre de réaliser des statistiques  et à vous proposer du contenu et des offres et services adaptés à vos centres d’intérêts. ",
+	"alertSmall": "Gestion des services",
+    "acceptAll": "OK, tout accepter",
+    "personalize": "Personnaliser",
+    "close": "Fermer",
+
+    "privacyUrl": "Politique de confidentialité",
+
+    "all": "Préférences pour tous les services",
+
+    "info": "Protection de votre vie privée",
+    "disclaimer": "En autorisant ces services tiers, vous acceptez le dépôt et la lecture de cookies et l'utilisation de technologies de suivi nécessaires à leur bon fonctionnement.",
+    "allow": "Autoriser",
+    "deny": "Interdire",
+    "noCookie": "Ce service ne dépose aucun cookie.",
+    "useCookie": "Ce service peut déposer",
+    "useCookieCurrent": "Ce service a déposé",
+    "useNoCookie": "Ce service n'a déposé aucun cookie.",
+    "more": "En savoir plus",
+    "source": "Voir le site officiel",
+    "credit": "Gestion des cookies par Adis",
+    "noServices": "Ce site n'utilise aucun cookie nécessitant votre consentement.",
+
+    "toggleInfoBox": "Afficher/masquer les informations sur le stockage des cookies",
+    "title": "Panneau de gestion des cookies",
+    "cookieDetail": "Détail des cookies",
+    "ourSite": "sur notre site",
+    "newWindow": "(nouvelle fenêtre)",
+    "allowAll": "Autoriser tous les cookies",
+    "denyAll": "Interdire tous les cookies",
+    
+    "fallback": "est désactivé.",
+
+     "necessaires": {
+        "title": "Nécessaires",
+        "details": "Les cookies nécessaires contribuent à rendre un site Web utilisable en activant des fonctions de base comme la navigation de page et l'accès aux zones sécurisées du site Web. Le site Web ne peut pas fonctionner correctement sans ces cookies."
+    },
+    "statistiques": {
+        "title": "Statistiques",
+        "details": "Les cookies statistiques aident les propriétaires du site Web, par la collecte et la communication d'informations de manière anonyme, à comprendre comment les visiteurs interagissent avec les sites Web."
+    },
+    "marketing": {
+        "title": "Marketing",
+        "details": "Les cookies marketing sont utilisés pour effectuer le suivi des visiteurs au travers des sites Web. Le but est d'afficher des publicités qui sont pertinentes et intéressantes pour l'utilisateur individuel et donc plus précieuses pour les éditeurs et annonceurs tiers."
+    },
+    "preferences": {
+        "title": "Préférences",
+        "details": "Les cookies de préférences permettent à un site Web de retenir des informations qui modifient la manière dont le site se comporte ou s’affiche, comme votre langue préférée ou la région dans laquelle vous vous situez."
+    }
+};
